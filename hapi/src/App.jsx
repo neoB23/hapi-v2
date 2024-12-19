a33c1e9280d9472a376f8a85717794670c9911ff
John Doe <johndoe@email.com>
2024-12-19 20:49:45 +0800

    asd

--- hapi/src/App.jsx
+++ hapi/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer'
 import Nav from './components/Nav'
-import Hero from './components/hero';
+import Hero from './components/Hero';
 import About from './components/About';
 import Pricing from './components/Pricing';
 import Contact from './components/Contact';
