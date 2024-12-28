d5f90d840e724d9852f812e06f61d7d3d770af52
John Doe <johndoe@email.com>
2024-12-28 12:36:16 +0800

    signup fix issue

--- hapi/src/components/Signup.jsx
+++ hapi/src/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Signup() {
     return (
@@ -131,5 +132,11 @@ function Signup() {
         </div>
     );
 }
-
+function CustomLink({ to, children, ...props }) {
+    return (
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    );
+  }
 export default Signup;
